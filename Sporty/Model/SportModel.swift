13e5b59b5ef7422fa03945cef8c0f1d850089116
mariam-elkirch <mariamk019@gmail.com>
2022-05-12 20:02:00 +0200

    splash

---
+++ Sporty/Model/SportModel.swift
@@ -0,0 +1,12 @@
+//
+//  SportModel.swift
+//  Sporty
+//
+//  Created by user188930 on 5/12/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import Foundation
+class SportsModel: Codable {
+    var sports: [Sport]?
+}
---
+++ Sporty/Services/Service.swift
@@ -0,0 +1,39 @@
+//
+//  Service.swift
+//  Sporty
+//
+//  Created by user188930 on 5/12/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import Foundation
+import  Alamofire
+protocol NetworkServiceProtocol{
+  static func sportResult(complitionHandler : @escaping (SportsModel?) -> Void)
+}
+class NetworkServic : NetworkServiceProtocol {
+    
+    static func sportResult(complitionHandler : @escaping (SportsModel?) -> Void){
+        Alamofire.request("https://www.thesportsdb.com/api/v1/json/2/all_sports.php")
+        .response{(responseData) in
+            guard let data = responseData.data else {
+               // self.callBack?(nil, false, "")
+                //print(data.)
+                return}
+            do {
+            let sports = try JSONDecoder().decode(SportsModel.self, from: data)
+               // self.callBack?(countries, true,"")
+                 complitionHandler(sports)
+                print(sports.sports?[2].strSport ?? "batot"  , "55")
+                print("hee")
+            } catch {
+                print(error)
+                complitionHandler(nil)
+               // self.callBack?(nil, false, error.localizedDescription)
+            }}
+        
+    }
+    
+    
+    
+}
