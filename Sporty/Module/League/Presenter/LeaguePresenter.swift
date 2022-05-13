af6d16b801db00596e06b7a501aa23e45578370b
hager <magdyhager70@gmail.com>
2022-05-13 06:16:36 -0700

    League skeleton

---
+++ Sporty/Module/League/Presenter/LeaguePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  LeaguePresenter.swift
+//  Sporty
+//
+//  Created by  Hager Magdy on 5/13/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import Foundation
---
+++ Sporty/Module/League/View/LeagueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueViewController.swift
+//  Sporty
+//
+//  Created by  Hager Magdy on 5/13/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import UIKit
+
+class LeagueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("League view controller")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
