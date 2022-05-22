f5de789ead12b2aa979e3ffba300530a7846490c
mariam-elkirch <mariamk019@gmail.com>
2022-05-22 09:53:40 +0200

    show favorites from coredata in table view

--- Sporty/Module/Home/View/FavouriteViewController.swift
+++ Sporty/Module/Home/View/FavouriteViewController.swift
@@ -17,6 +17,7 @@ class FavouriteViewController: UIViewController ,UITableViewDelegate,UITableView
    //  var dataModel : LocalDataModel?
      var presenter = FavouritePresenter()
      var favouriteResult: [FavourieLeague] = []
+      var urlSelected : String?
     @IBOutlet weak var favTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,14 +45,49 @@ class FavouriteViewController: UIViewController ,UITableViewDelegate,UITableView
           }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
           let cellFav = tableView.dequeueReusableCell(withIdentifier: "cellFav", for: indexPath) as! FavTableViewCell
-        //cellFav.labelFav.adjustsFontSizeToFitWidth = true
-        //cellFav.labelFav.minimumScaleFactor = 2.5
-        cellFav.labelFav.text = "mmmmmmmmmm"//favouriteResult[indexPath.row].strLeague
-        
-        cellFav.imgFav.image = UIImage(named: "imgball")
+        cellFav.labelFav.adjustsFontSizeToFitWidth = true
+        cellFav.labelFav.minimumScaleFactor = 0.49
+         cellFav.labelFav.preferredMaxLayoutWidth = 150
+        cellFav.labelFav.text = favouriteResult[indexPath.row].strLeague
+       
+      let url=URL(string:self.favouriteResult[indexPath.row].strBadge ??
+                "https://www.thesportsdb.com//images//media//league//badge//6my1u31578828133.png" )
+                let res=ImageResource(downloadURL:url!)
+
+        cellFav.imgFav?.kf.setImage(with: res, placeholder: UIImage(named: "nemo.jpg"))
+                
+              cellFav.imgFav?.layer.cornerRadius = (cellFav.imageView?.frame.size.width)!/2
+              cellFav.imgFav?.layer.masksToBounds = true
+               cellFav.imgFav?.layer.borderWidth = 2
+                if (favouriteResult[indexPath.row].strYoutube ==  ""){
+                    cellFav.btnFavYou.isHidden = true
+                }
+                else {
+                    cellFav.btnFavYou.tag = indexPath.row
+                 cellFav.btnFavYou.addTarget(self, action: #selector(openYoutube(sender: )), for: .touchUpInside)
+                urlSelected = favouriteResult[indexPath.row].strYoutube
+                }
         return cellFav
 
     }
+     @objc
+        func openYoutube(sender : UIButton ){
+            if let url = URL(string : "https://\(urlSelected ?? "https://www.google.com")"){
+                print(urlSelected!)
+                UIApplication.shared.open(url , options : [:]){
+                    (done) in
+    //                print("Opend sucsesfully")
+                    if done {
+                               print("opened")
+                           } else {
+                               print("failed")
+                               // showInvalidUrlAlert()
+                           }
+                }
+            }
+            print("button")
+        }
+        
     func displayFavouriteLeagues() {
     // presenter.fetchLeaguesFromCoreData()
          // favouriteResult = presenter.resultLeagueCoreData
